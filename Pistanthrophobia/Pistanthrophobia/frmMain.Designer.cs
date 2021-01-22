21f750828061fb11f594d1468ac510eff840c126
Ray-Arthur <27481892+StarZHF@users.noreply.github.com>
2021-01-21 18:12:26 -0600

    Add files via upload

---
+++ Pistanthrophobia/Pistanthrophobia/frmMain.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Pistanthrophobia
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnCheat = new System.Windows.Forms.Button();
+            this.lblGhostInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheat
+            // 
+            this.btnCheat.Location = new System.Drawing.Point(25, 77);
+            this.btnCheat.Name = "btnCheat";
+            this.btnCheat.Size = new System.Drawing.Size(75, 23);
+            this.btnCheat.TabIndex = 0;
+            this.btnCheat.Text = "Cheat";
+            this.btnCheat.UseVisualStyleBackColor = true;
+            this.btnCheat.Click += new System.EventHandler(this.btnCheat_Click);
+            // 
+            // lblGhostInfo
+            // 
+            this.lblGhostInfo.AutoSize = true;
+            this.lblGhostInfo.Location = new System.Drawing.Point(12, 9);
+            this.lblGhostInfo.Name = "lblGhostInfo";
+            this.lblGhostInfo.Size = new System.Drawing.Size(0, 13);
+            this.lblGhostInfo.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(124, 108);
+            this.Controls.Add(this.lblGhostInfo);
+            this.Controls.Add(this.btnCheat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Pistanthrophobia | UID: 69588";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheat;
+        private System.Windows.Forms.Label lblGhostInfo;
+    }
+}
+
---
+++ Pistanthrophobia/Pistanthrophobia/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace Pistanthrophobia
+{
+    public partial class frmMain : Form
+    {
+        public string saveDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\LocalLow\Kinetic Games\Phasmophobia";
+        public string saveFileName = @"saveData.txt";
+        public string saveFilePassword = "CHANGE ME TO YOUR OWN RANDOM STRING"; // Literally what the dev used. copypasta++
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            MaximumSize = Size;
+            MinimumSize = Size;
+        }
+
+        private void btnCheat_Click(object sender, EventArgs e)
+        {
+            lblGhostInfo.Text = getRoute(getGhostType());
+        }
+
+        public string getGhostType()
+        {
+            try
+            {
+                string tempFilePath = Path.Combine(saveDirectory, saveFileName);
+
+                string data = File.ReadAllText(tempFilePath); 
+
+                StringBuilder text = new StringBuilder();
+                for (int i = 0; i < data.Length; i++) // Decode the safe file
+                {
+                    text.Append((char)(data[i] ^ saveFilePassword[i % saveFilePassword.Length]));
+                }
+
+                return System.Text.RegularExpressions.Regex.Match(text.ToString(), @"GhostType\""\,\""Value\""\:\""(\w{3,12})").Groups[1].ToString(); // You could go through the hassle of implementing JSON Parsing
+                                                                                                                                                      // for one line, However, I would say RegEx suffices.
+            }
+            catch (Exception ex)
+            {
+                return $"Something fucked up.\r\n {ex.Message}"; // Top tier error logging.
+            }
+        }
+
+        public string getRoute(string ghostType) // Dirty, Works for the small scale project.
+        {
+            foreach(string json in File.ReadAllLines("ghosts.info"))
+            {
+                if (!json.Contains(ghostType)) { continue; } // If it isn't our ghost; skip until it is...
+
+                Ghost jsGhost = new Ghost(); // tbh this is just me playing with the new native JSON framework..
+                jsGhost = JsonSerializer.Deserialize<Ghost>(json);
+
+                return $"{jsGhost.ghostType}\r\n\r\n{jsGhost.ghostEvidence}"; // And finally return our information.
+            }
+            return ghostType; // If the ghost isn't in our list, just tell us the type.
+        }
+        public class Ghost
+        {
+            public string ghostType { get; set; }
+
+            public string ghostEvidence { get; set; }
+        }
+    }
+}
